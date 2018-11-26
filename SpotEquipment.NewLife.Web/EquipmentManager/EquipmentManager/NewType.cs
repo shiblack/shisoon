a94904a948cc21c33566047e568d22c18c86dc60
shiblack <1981741604@qq.com>
2018-11-26 21:00:46 +0800

    enum

---
+++ SpotEquipment.NewLife.Web/EquipmentManager/EquipmentManager/NewType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentManager
+{
+    public enum NewType
+    {
+        //计量精度等级
+        A级=0,
+        //
+        B级=1,
+        //
+        C级=2,
+        //
+        D级=3
+    }
+}
